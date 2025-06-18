ccb83281114c5afad9d3a037fa28187d678b975b
Sara-Chaudhary <sara.siwaya@gmail.com>
2025-06-18 12:25:40 +0530

    updated the upload pdf api

--- Router/home.py
+++ Router/home.py
@@ -3,9 +3,9 @@ from fastapi.responses import JSONResponse ,RedirectResponse
 from .auth import get_current_user
 from typing import Annotated
 from fastapi.templating import Jinja2Templates
-from tempfile import NamedTemporaryFile
 import shutil
 import redis
+import os
 from celery.result import AsyncResult
 from .task import make_qdrant
 from celery_app import celery
@@ -34,26 +34,27 @@ async def render_home_page(user:user_dependency,request:Request):
 async def user_home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
     
-    
-# api to upload pdf
+
 @router.post("/uploadfiles", status_code=status.HTTP_201_CREATED)  
-async def upload_pdf(user:Annotated[dict,Depends(get_current_user)] ,file: UploadFile = File(...)):
+async def upload_pdf(user: Annotated[dict, Depends(get_current_user)], file: UploadFile = File(...)):
     if user is None:
         raise HTTPException(status_code=401, detail='Authentication Failed')
     if not file.filename.endswith(".pdf"):
         return JSONResponse(status_code=400, content={"error": "Only PDF files are supported."})
+    
+    SHARED_DIR = "/app/shared"
+    os.makedirs(SHARED_DIR, exist_ok=True) 
+    file_path = os.path.join(SHARED_DIR, file.filename)
 
     try:
-        with NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-            shutil.copyfileobj(file.file, tmp_file)
-            tmp_file_path = tmp_file.name
-        # Send task to Celery
-        task = make_qdrant.delay(tmp_file_path)
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        task = make_qdrant.delay(file_path)
         return {"message": "Processing started", "task_id": task.id}
     
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
+    
 @router.get("/status/{task_id}")
 def get_status(user:user_dependency,task_id: str):
     if user is None:
