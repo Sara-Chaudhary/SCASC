45ed954bf142e3c1470723744095fc979764faca
Sara-Chaudhary <sara.siwaya@gmail.com>
2025-06-19 12:54:20 +0530

    deleted extra folder

--- ubuntu@34.205.30.246/celery_app.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-
-celery= Celery(
-    "tasks",
-    backend="redis://localhost:6379/0",
-    broker="redis://localhost:6379/0"
-)
-
-import Router.task
--- ubuntu@34.205.30.246/test/test_main.py
+++
@@ -1,6 +0,0 @@
-from main import client
-
-def test_get_data():
-    response = client.get()
-    assert response.status_code == 200
-    assert response.text == "Data"
